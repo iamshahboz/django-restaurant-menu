a2a5f719dd5b1d775f303b768e3890d0d39e4639
Shahboz <sharafzodashahboz@gmail.com>
2023-12-06 11:59:24 +0500

    models defined

--- menu/models.py
+++ menu/models.py
@@ -1,21 +1,23 @@
 from django.db import models
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=100)
+class Menu(models.Model):
+    name = models.CharField('Название меню', max_length=100, unique=True)
+    description = models.TextField('Описание', null=True, blank=True)
 
     def __str__(self):
         return self.name 
     
     class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
+        verbose_name = 'Название меню'
+        verbose_name_plural = 'Название меню'
 
 class MenuItem(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=8,decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField('Пункт меню', max_length=50, unique=True)
+    description = models.TextField('Описание', blank=True, null=True)
+    url = models.URLField('Ссылка', max_length=200)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='menu_items')
 
     def __str__(self):
         return self.name 
