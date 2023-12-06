48715b62e485a7ac942348c46e2532fe1d412697
Shahboz <sharafzodashahboz@gmail.com>
2023-12-06 12:34:10 +0500

    class based view created

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import View 
+from .models import Menu 
+from django.http import HttpResponse
 
-# Create your views here.
+class IndexView(View):
+    template_name = 'menu/index.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'menus': Menu.objects.all()})
+
+
+class DrawMenuView(View):
+    template_name = 'menu/index.html'
+
+    def get(self,request, *args, **kwargs):
+        path = kwargs.get('path','')
+        splitted_path = path.split('/')
+        assert len(splitted_path) > 0, ('= Draw_menu function failed =')
+        print(splitted_path)
+        return render(request, self.template_name, {'menu_name': splitted_path[0], 'menu_item': splitted_path[-1]})
